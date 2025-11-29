79913d1bd82c18abdec06029ea2c43e185885202
peet5228 <ppeet5228@gmail.com>
2025-11-28 19:38:53 -0800

    pppp

---
+++ frontend/src/LoginForm.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-container fluid class="fill-height">
+        <v-row justify="center" align="center">
+             <v-col cols="12" md="6" lg="4">
+                <v-card elevation="10" rounded="lg" color="#7d0c14">
+                    <p class="text-center text-h5 font-weight-bold text-white">NTC EVALUATION SYSTEM</p>
+                    <p class="text-center text-white">ระบบประเมินประเมินบุคลากรเบื้องต้น</p>
+                    <v-form @submit.prevent=""></v-form>
+                </v-card>
+             </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
