cbb6c930fd3164e4bb79bd9700226b2b7a7cb850
peet5228 <ppeet5228@gmail.com>
2025-11-28 19:27:56 -0800

    update 2

---
+++ backend/routes/auth.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const db = require('../db')
+const router = express.Router()
+const jwt = require('jsonwebtoken')
+const JWT_SECRET = process.env.JWT_SECRET
+
+router.post('/login',async (req,res) => {
+    try{
+        const {username,password,role} = req.body
+        const [rows] = await db.query(`select * from tb_member where username=? and password=? and role=?`,[username,password,role])
+        const m = jwt.sign(
+            {id_member:m.id_member,username:m.username,role:m.role} , JWT_SECRET , {expiresIn:'2h'}
+        )
+        res.json({token,role:m.role})
+    }catch(err){
+        console.error('Login Failed',err)
+        res.status(500).json({message:'Login Failed'})
+    }
+})
+
+router.post('/regis',async (req,res) => {
+    try{
+        const {first_name,last_name,email,username,password,role} = req.body
+        const [rows] = await db.query(`insert into tb_member (first_name,last_name,email,username,password,role) values (?,?,?,?,?,?)`,[first_name,last_name,email,username,password,role])
+        res.json({message:'Register Success'})
+    }catch(err){
+        console.error('Error Register',err)
+        res.status(500).json({message:'Error Register'})
+    }
+})
+
+module.exports = router
